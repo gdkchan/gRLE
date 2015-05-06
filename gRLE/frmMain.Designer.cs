14c7cf552f1908e886bcfd9a968846d61be97109
gdkchan <gab.dark.100@gmail.com>
2015-05-06 10:54:15 -0300

    O hai

---
+++ gRLE/frmMain.Designer.cs
@@ -0,0 +1,173 @@
+﻿namespace gRLE
+{
+    partial class frmMain
+    {
+        /// <summary>
+        /// Variável de designer necessária.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpar os recursos que estão sendo usados.
+        /// </summary>
+        /// <param name="disposing">verdade se for necessário descartar os recursos gerenciados; caso contrário, falso.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region código gerado pelo Windows Form Designer
+
+        /// <summary>
+        /// Método necessário para suporte do Designer - não modifique
+        /// o conteúdo deste método com o editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(frmMain));
+            this.button1 = new System.Windows.Forms.Button();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.groupBox1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Image = ((System.Drawing.Image)(resources.GetObject("button1.Image")));
+            this.button1.Location = new System.Drawing.Point(524, 12);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(88, 24);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "&Abrir";
+            this.button1.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
+            this.button1.TextImageRelation = System.Windows.Forms.TextImageRelation.ImageBeforeText;
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.pictureBox1);
+            this.groupBox1.Location = new System.Drawing.Point(12, 42);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(600, 388);
+            this.groupBox1.TabIndex = 1;
+            this.groupBox1.TabStop = false;
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Location = new System.Drawing.Point(6, 19);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(512, 240);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.AutoSize;
+            this.pictureBox1.TabIndex = 2;
+            this.pictureBox1.TabStop = false;
+            // 
+            // button2
+            // 
+            this.button2.Enabled = false;
+            this.button2.Image = ((System.Drawing.Image)(resources.GetObject("button2.Image")));
+            this.button2.Location = new System.Drawing.Point(236, 12);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(88, 24);
+            this.button2.TabIndex = 2;
+            this.button2.Text = "&Exportar";
+            this.button2.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
+            this.button2.TextImageRelation = System.Windows.Forms.TextImageRelation.ImageBeforeText;
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.Image = ((System.Drawing.Image)(resources.GetObject("button3.Image")));
+            this.button3.Location = new System.Drawing.Point(330, 12);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(88, 24);
+            this.button3.TabIndex = 3;
+            this.button3.Text = "&Importar";
+            this.button3.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
+            this.button3.TextImageRelation = System.Windows.Forms.TextImageRelation.ImageBeforeText;
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // button4
+            // 
+            this.button4.Enabled = false;
+            this.button4.Image = ((System.Drawing.Image)(resources.GetObject("button4.Image")));
+            this.button4.Location = new System.Drawing.Point(430, 12);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(88, 24);
+            this.button4.TabIndex = 4;
+            this.button4.Text = "&Salvar";
+            this.button4.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
+            this.button4.TextImageRelation = System.Windows.Forms.TextImageRelation.ImageBeforeText;
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Segoe UI", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(12, 18);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(46, 13);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "Lagura:";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(64, 15);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(64, 22);
+            this.textBox1.TabIndex = 3;
+            this.textBox1.Text = "512";
+            // 
+            // frmMain
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.WhiteSmoke;
+            this.ClientSize = new System.Drawing.Size(624, 442);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.button1);
+            this.Font = new System.Drawing.Font("Segoe UI", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.MaximizeBox = false;
+            this.Name = "frmMain";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "gRLE v0.1 by gdkchan";
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox textBox1;
+    }
+}
+
---
+++ gRLE/frmMain.cs
@@ -0,0 +1,212 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Drawing.Imaging;
+using System.Runtime.InteropServices;
+using System.IO;
+using System.Windows.Forms;
+
+namespace gRLE
+{
+    public partial class frmMain : Form
+    {
+        public frmMain()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog dlgOpen = new OpenFileDialog();
+            dlgOpen.Filter = "Imagem RLE|*.rle";
+            if (dlgOpen.ShowDialog() == System.Windows.Forms.DialogResult.OK && File.Exists(dlgOpen.FileName))
+            {
+                byte[] data = RLEDecompress(new FileStream(dlgOpen.FileName, FileMode.Open));
+
+                int width = 512;
+                int.TryParse(textBox1.Text, out width);
+
+                Bitmap img = new Bitmap(width, (data.Length / width) / 2);
+                for (int y = 0; y < img.Height; y++)
+                {
+                    for (int x = 0; x < img.Width; x++)
+                    {
+                        int o = ((y * img.Width) + x) * 2;
+
+                        ushort val = BitConverter.ToUInt16(data, o);
+                        byte B = (byte)((val >> 10) & 0x1f);
+                        byte G = (byte)((val >> 5) & 0x1f);
+                        byte R = (byte)((val) & 0x1f);
+
+                        R = (byte)((R << 3) | (R >> 2));
+                        G = (byte)((G << 3) | (G >> 2));
+                        B = (byte)((B << 3) | (B >> 2));
+
+                        img.SetPixel(x, y, Color.FromArgb(R, G, B));
+                    }
+                }
+
+                pictureBox1.Image = img;
+                button2.Enabled = true;
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            SaveFileDialog saveDlg = new SaveFileDialog();
+            saveDlg.Filter = "Imagem|*.png";
+            if (saveDlg.ShowDialog() == System.Windows.Forms.DialogResult.OK)
+            {
+                pictureBox1.Image.Save(saveDlg.FileName);
+            }
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog dlgOpen = new OpenFileDialog();
+            dlgOpen.Filter = "Imagem|*.png";
+            if (dlgOpen.ShowDialog() == System.Windows.Forms.DialogResult.OK && File.Exists(dlgOpen.FileName))
+            {
+                pictureBox1.Image = new Bitmap(dlgOpen.FileName);
+                button4.Enabled = true;
+            }
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            SaveFileDialog saveDlg = new SaveFileDialog();
+            saveDlg.Filter = "Imagem RLE|*.rle";
+            if (saveDlg.ShowDialog() == System.Windows.Forms.DialogResult.OK)
+            {
+                Bitmap img = new Bitmap(pictureBox1.Image);
+
+                MemoryStream rawImage = new MemoryStream();
+                BinaryWriter writer = new BinaryWriter(rawImage);
+                for (int y = 0; y < img.Height; y++)
+                {
+                    for (int x = 0; x < img.Width; x++)
+                    {
+                        Color color = img.GetPixel(x, y);
+
+                        ushort val = (ushort)(((color.R & 0xf8) >> 3) | ((color.G & 0xf8) << 2) | ((color.B & 0xf8) << 7));
+                        writer.Write(val);
+                    }
+                }
+                rawImage.Seek(0, SeekOrigin.Begin);
+
+                byte[] RLEData = RLECompress(rawImage);
+                File.WriteAllBytes(saveDlg.FileName, RLEData);
+            }
+        }
+
+        private byte[] RLEDecompress(Stream input)
+        {
+            BinaryReader reader = new BinaryReader(input);
+            MemoryStream output = new MemoryStream();
+            String signature = null;
+            for (int i = 0; i < 8; i++)
+            {
+                signature += Convert.ToChar(reader.ReadByte());
+            }
+            if (signature != "_RLE_16_")
+            {
+                MessageBox.Show("Arquivo inválido!", "Erro!", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                return null;
+            }
+
+            uint decompressedLength = reader.ReadUInt32();
+            while (input.Position < input.Length)
+            {
+                ushort header = reader.ReadUInt16();
+                ushort length = (ushort)(header & 0x7fff);
+                if (length == 0) break;
+     
+                if ((header & 0x8000) != 0)
+                {
+                    ushort data = reader.ReadUInt16();
+                    for (int i = 0; i < length; i++)
+                    {
+                        output.WriteByte((byte)(data & 0xff));
+                        output.WriteByte((byte)(data >> 8));
+                    }
+                }
+                else
+                {
+                    for (int i = 0; i < length; i++)
+                    {
+                        ushort data = reader.ReadUInt16();
+                        output.WriteByte((byte)(data & 0xff));
+                        output.WriteByte((byte)(data >> 8));
+                    }
+                }
+
+            }
+
+            input.Close();
+            return output.ToArray();
+        }
+
+        private byte[] RLECompress(Stream input)
+        {
+            BinaryReader reader = new BinaryReader(input);
+
+            MemoryStream output = new MemoryStream();
+            BinaryWriter writer = new BinaryWriter(output);
+            writer.Write(Encoding.ASCII.GetBytes("_RLE_16_"));
+            writer.Write((uint)input.Length); //O jogo não usa isso, e sim o EOF
+
+            bool EOF = false;
+            while (input.Position < input.Length - 1)
+            {
+                ushort data = reader.ReadUInt16();
+                if (EOF || input.Position >= input.Length - 1)
+                {
+                    writer.Write((ushort)1);
+                    writer.Write(data);
+                    break;
+                }
+                ushort length = 0;
+
+                while (input.Position < input.Length - 1 && reader.ReadUInt16() == data && length < 0x7ffe) length++;
+                if (input.Position >= input.Length - 1) EOF = true;
+                input.Seek(-2, SeekOrigin.Current);
+                if (length > 0)
+                {
+                    writer.Write((ushort)(0x8000 | (length + 1)));
+                    writer.Write(data);
+                    length = 0;
+                }
+                else
+                {
+                    bool different = true;
+                    long headerPosition = output.Position;
+                    writer.Write((ushort)0); //Reserva espaço do header
+                    while (different && length < 0x7fff && input.Position < input.Length - 1)
+                    {
+                        ushort nextData = reader.ReadUInt16();
+                        different = data != nextData;
+                        writer.Write(data);
+                        length++;
+                        data = nextData;
+                    }
+                    if (input.Position >= input.Length - 1) EOF = true;
+                    input.Seek(-2, SeekOrigin.Current);
+                    long position = output.Position;
+                    output.Seek(headerPosition, SeekOrigin.Begin);
+                    writer.Write((ushort)length);
+                    output.Seek(position, SeekOrigin.Begin);
+                }
+            }
+
+            writer.Write((ushort)0); //EOF
+
+            input.Close();
+            return output.ToArray();
+        }
+    }
+}
